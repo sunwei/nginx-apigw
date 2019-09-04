476f511b73b146dd6e208327b040f6c2e53c8446
sunzhongmou <sunzhongmou820@163.com>
2019-09-04 16:19:35 +0800

    Modified: deploy to heroku; Wayde

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -4,6 +4,7 @@ set -e
 source secrets/heroku.env
 
 echo "> Pushing image to heroku..."
-docker login --username=${HEROKU_LOGIN} --password=${HEROKU_TOKEN} registry.heroku.com
+echo "${HEROKU_TOKEN}" | \
+docker login --username="${HEROKU_LOGIN}" --password-stdin registry.heroku.com
 docker tag sunzhongmou/nginx-apigw:latest registry.heroku.com/oneapi-stg/web:latest
 docker push registry.heroku.com/oneapi-stg/web:latest

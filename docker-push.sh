176f681f39cea9ddea7bfd21ddedb9b2570c8da9
sunzhongmou <sunzhongmou820@163.com>
2019-09-04 11:27:33 +0800

    Modified: remove dependence from git-crypt; Wayde

--- docker-push.sh
+++ docker-push.sh
@@ -5,7 +5,7 @@ get_commit_count() {
   git rev-list --all --count
 }
 
-echo secrets/docker-hub.env
+cat secrets/docker-hub.env | echo
 source secrets/docker-hub.env
 echo "> Logging docker hub with user: ${DOCKER_USERNAME}"
 

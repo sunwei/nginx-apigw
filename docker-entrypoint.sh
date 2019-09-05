1fffc6c62d4dd3e77d1482e5dc2b7802a29a0d73
sunzhongmou <sunzhongmou820@163.com>
2019-09-05 16:47:39 +0800

    change port; Wayde

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
-sed -i -e 's/HEROKU_DYNO_PORT/${PORT}/g' ./nginx/api_gw.d/api.oneapi.cc2.conf
+sed -i -e 's/HEROKU_DYNO_PORT/${PORT}/g' /nginx/api_gw.d/api.oneapi.cc2.conf
+
+cat /nginx/api_gw.d/api.oneapi.cc2.conf
 
 exec "$@"

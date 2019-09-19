93a69f445a3ea51e8725473f0fb75c365abc4ae4
sunzhongmou <sunzhongmou820@163.com>
2019-09-19 16:53:22 +0800

    Modified: add for dyno port; Wayde

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-sed -i -e 's/HEROKU_DYNO_PORT/'"${PORT}"'/g' /etc/nginx/api_gw.d/api.oneapi.cc.conf
+sed -i -e 's/HEROKU_DYNO_PORT/'"${PORT}"'/g' /etc/nginx/api_gw.d/*.conf
 sed -i -e 's/HEROKU_DYNO_PORT/'"${PORT}"'/g' /etc/nginx/conf.d/default.conf
 
 cat /etc/nginx/api_gw.d/api.oneapi.cc.conf

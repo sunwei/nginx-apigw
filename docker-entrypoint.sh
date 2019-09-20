15c5dc841f47d88085449b91c760bc5824e35c1a
sunzhongmou <sunzhongmou820@163.com>
2019-09-20 10:38:57 +0800

    Modified: config for nginx; Wayde

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 
+sed -i -e 's/listen 80;/listen HEROKU_DYNO_PORT;/g' /etc/nginx/api_gw.d/*.conf
 sed -i -e 's/HEROKU_DYNO_PORT/'"${PORT}"'/g' /etc/nginx/api_gw.d/*.conf
 sed -i -e 's/HEROKU_DYNO_PORT/'"${PORT}"'/g' /etc/nginx/conf.d/default.conf
-
-cat /etc/nginx/api_gw.d/api.oneapi.cc.conf
 
 exec "$@"
